b8f46afc7390a95ec98f981ede10862bbb5cba58
Thaynara Araújo <thaynara_araujo1@yahoo.com.br>
2022-02-02 22:13:10 -0300

    criação de user model

--- src/db.ts
+++ src/db.ts
@@ -1,6 +1,6 @@
 import { Pool } from "pg";
 
-const connectionString = '';
+const connectionString = `postgres://${process.env.USER}:${process.env.PGPASSWORD}@kesavan.db.elephantsql.com/ffyjeazw`;
 const db = new Pool({ connectionString });
 
 export default db;
---
+++ src/repositories/user.repository.ts
@@ -0,0 +1,11 @@
+import User from "../models/user.models";
+
+class UserRepository {
+
+    findAllUsers(): User[] {
+        return [];
+    }
+        
+}
+
+export default new UserRepository();
